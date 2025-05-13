08fb55d35287c32c64a9076dc2df952efe570d02
Mazak2 <110530910+Mazak2@users.noreply.github.com>
2025-05-13 20:13:25 +0200

    Props, fuse box now turns off lights

---
+++ FuseBox_Body.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+var turned_on = true
+@export var lobby_lights = OmniLight3D
+@export var corridor_lights = OmniLight3D
+@export var underground_lights = OmniLight3D
+@export var underground_lights_2 = OmniLight3D
+@export var	desk_lamp = SpotLight3D
+
+
+func interact():
+	if turned_on == true:
+		lobby_lights.light_energy = 0
+		corridor_lights.light_energy = 0
+		underground_lights.light_energy = 0
+		underground_lights_2.light_energy = 0
+		desk_lamp.light_energy = 0
+		turned_on = false
+	elif turned_on == false:
+		lobby_lights.light_energy = 10.51
+		corridor_lights.light_energy = 3.101
+		underground_lights.light_energy = 10.51
+		underground_lights_2.light_energy = 10.51
+		desk_lamp.light_energy = 4.394
+		turned_on = true
+		
